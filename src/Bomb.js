292014252fc3defb6d00a9ab68604de65953ba26
Christine Contreras <christineec23@yahoo.com>
2021-06-04 10:06:33 -0500

    lab passed

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,27 @@
-// your Bomb code here!
+import React, { Component } from 'react';
+
+class Bomb extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount 
+        }
+    }
+
+    renderMessage = () => {
+        return this.state.secondsLeft === 0 ? <p>Boom!</p> : <p>{this.state.secondsLeft} seconds left before I go boom!</p>
+    }
+
+
+    render() {
+        
+        return (
+            <div>
+            {this.renderMessage()}
+            </div>
+        )
+
+    }
+}
+
+export default Bomb;
